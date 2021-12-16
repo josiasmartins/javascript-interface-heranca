e0e0a6d09d2f4efbfdcb16a335d3ca51991a8faa
Caitano <josiasmartinscaitano@gmail.com>
2021-12-16 18:32:41 -0300

    operator in, operator instaceof, duck type

--- Cliente.js
+++ Cliente.js
@@ -10,6 +10,11 @@ export class Cliente{
         this.nome = nome;
         this._cpf = cpf;
         this._senha = senha;
+        this.autenticar = 1;
+    }
+
+    autenticar() {
+        return true;
     }
 
 }
--- sistemaAutenticacao.js
+++ sistemaAutenticacao.js
@@ -5,6 +5,16 @@
 export class SistemaAutenticacao {
 
     static login(autenticavel, senha) {
-        return autenticavel.autenticar(senha);
+        if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    // chamo ele direto, porque ele é um método static
+    static ehAutenticavel(autenticavel) {
+        // operator in: verifica se a chave ('autenticar') existe dentro do objeto (autenticavel)
+        // instaceof: se é uma instancia de uma função
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
